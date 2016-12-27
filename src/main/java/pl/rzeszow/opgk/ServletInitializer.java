78e76d7b2cc329bf2527179157bc83bd7e4c07b8
Kuba <kicek@kicek.org>
2016-12-27 10:23:51 +0100

    intial project commit

---
+++ src/main/java/pl/rzeszow/opgk/ServletInitializer.java
@@ -0,0 +1,13 @@
+package pl.rzeszow.opgk;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(CommerceApplication.class);
+	}
+
+}
