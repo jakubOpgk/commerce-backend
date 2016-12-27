fe834d60a8dd69ab99901c3323e2a766094f8484
Kuba <kzajac@opgk.rzeszow.pl>
2016-12-27 15:02:29 +0100

    simple test with fail result

---
+++ src/main/java/pl/rzeszow/opgk/config/ApplicationConfiguration.java
@@ -0,0 +1,10 @@
+package pl.rzeszow.opgk.config;
+
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * Created by Kuba on 27.12.2016.
+ */
+@Configuration
+public class ApplicationConfiguration {
+}
--- src/test/java/pl/rzeszow/opgk/repository/AuctionRepositoryTest.java
+++ src/test/java/pl/rzeszow/opgk/repository/AuctionRepositoryTest.java
@@ -1,7 +1,50 @@
 package pl.rzeszow.opgk.repository;
 
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
 /**
  * Created by Kuba on 27.12.2016.
  */
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringBootTest
 public class AuctionRepositoryTest {
+
+    @Autowired
+    AuctionRepository auctionRepository;
+
+    @Before
+    public void clean() {
+        auctionRepository.deleteAll();
+    }
+
+    @Test
+    public void shouldBeFailToSaveWithoutDescription() {
+        Assert.assertTrue(false);
+    }
+
+    @Test
+    public void shouldBeSuccessToSave() {
+
+    }
+
+    @Test
+    public void shouldBeFailToUpdateWithoutDescription() {
+
+    }
+
+    @Test
+    public void shouldBeSuccessToUpdate() {
+
+    }
+
+    @Test
+    public void shouldBeSuccessToDelete() {
+
+    }
 }
